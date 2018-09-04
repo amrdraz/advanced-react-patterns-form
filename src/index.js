89f785640b2c61a1e689533131fe00c19604a8ad
Amr Draz <amrdraz@users.noreply.github.com>
2018-09-04 21:07:38 +0200

    saving at start state
    just making sure the first page is the one that's open

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // 📋Overview and starting point
-// import { Page } from "./examples/01";
+import { Page } from "./examples/01";
 // 📋Creating Compound Components and Form.Field Component
 // import { Page } from "./examples/02";
 // 📋RenderProp and Flexible Compunt Connecting with Context
 // import { Page } from "./examples/03";
 // 📋User HigherOrder Components to ease using our Form
-import { Page } from "./examples/04";
+// import { Page } from "./examples/04";
 import "./styles.css";
 
 function App() {
